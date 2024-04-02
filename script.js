8a3c3aefef2cbc81e6003d76577593e25df264c8
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-04-02 09:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+   btn.addEventListener('click',function()){
+	   let p1 = downloadImg(images[0].url);
+	   let p2= downloadImg(images[1].url);
+	   let p3 = downloadImg(images[2].url);
+	   let promiseArr = Promise.all([p1,p2,p3]);
+	   promiseArr.then((img)=>{
+		   for(let i=0;i<img.length;i++){
+			   output.appendChild(img[i]);
+		   }
+	   }).catch((arr)=>{
+		   console.log(arr);
+	   });
+	   function downloadImage(url){
+	let img = new Image();
+	return new Promise((resolve,reject)=>{
+		img.onload=function(){
+			resolve(img);
+		}
+		img.onerror=function(){
+			reject(new Error(Failed to load image's URL : ${url}));
+		}
+	img.src = url;
+	})
+
+   }
